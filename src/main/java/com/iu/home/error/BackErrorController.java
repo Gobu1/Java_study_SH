3f6fedd1aa1f3419548f0c67144ec2089f28eff1
Gobu1 <gogkgngg@gmail.com>
2022-09-13 15:41:36 +0900

    111

---
+++ src/main/java/com/iu/home/error/BackErrorController.java
@@ -0,0 +1,5 @@
+package com.iu.home.error;
+
+public class BackErrorController {
+
+}
--- src/main/java/com/iu/home/interceptor/MemberInterceptor.java
+++ src/main/java/com/iu/home/interceptor/MemberInterceptor.java
@@ -4,10 +4,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-public class MemberInterceptor extends HandlerInterceptorAdapter {
+import com.iu.home.bankMembers.BankMembersDAO;
+import com.iu.home.bankMembers.BankMembersDTO;
 
+public class MemberInterceptor extends HandlerInterceptorAdapter {
+	
+	@Autowired
+	private BankMembersDAO bankMembersDAO;
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
